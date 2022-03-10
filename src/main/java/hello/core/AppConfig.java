481cd41ff4a862fb23d80de6d8f0f4d8e50bc24f
minsoozz <minsoo1604@naver.com>
2022-03-10 21:32:22 +0900

    스프링으로 전환하기

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -7,22 +7,29 @@ import hello.core.order.OrderServiceImpl;
 import hello.member.MemberService;
 import hello.member.MemberServiceImpl;
 import hello.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+  @Bean
   public MemberService memberService() {
     return new MemberServiceImpl(memberRepository());
   }
 
-  private MemoryMemberRepository memberRepository() {
+  @Bean
+  public MemoryMemberRepository memberRepository() {
     return new MemoryMemberRepository();
   }
 
+  @Bean
   public OrderService orderService() {
     return new OrderServiceImpl(memberRepository(), discountPolicy());
   }
 
-  private DiscountPolicy discountPolicy() {
+  @Bean
+  public DiscountPolicy discountPolicy() {
 //    return new FixDiscountPolicy();
     return new RateDiscountPolicy();
   }
--- src/main/java/hello/core/order/OrderApp.java
+++ src/main/java/hello/core/order/OrderApp.java
@@ -4,15 +4,21 @@ import hello.core.AppConfig;
 import hello.member.Grade;
 import hello.member.Member;
 import hello.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
   public static void main(String[] args) {
 
-    AppConfig appConfig = new AppConfig();
+//    AppConfig appConfig = new AppConfig();
+//    MemberService memberService = appConfig.memberService();
+//    OrderService orderService = appConfig.orderService();
 
-    MemberService memberService = appConfig.memberService();
-    OrderService orderService = appConfig.orderService();
+    ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+    OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
     Long memberId = 1L;
     Member member = new Member(memberId, "memberA", Grade.VIP);
