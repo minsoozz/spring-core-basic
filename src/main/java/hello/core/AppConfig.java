ba623a3e98bfad9b872602a27d3d114c42192ed0
minsoozz <minsoo1604@naver.com>
2022-03-17 20:00:11 +0900

    @Configuration과 싱글톤

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -2,35 +2,47 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
-import hello.core.order.OrderService;
-import hello.core.order.OrderServiceImpl;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
 
-  @Bean
-  public MemberService memberService() {
-    return new MemberServiceImpl(memberRepository());
-  }
+    //@Bean memberService -> new MemoryMemberRepository()
+    //@Bean orderService -> new MemoryMemberRepository()
 
-  @Bean
-  public MemoryMemberRepository memberRepository() {
-    return new MemoryMemberRepository();
-  }
+    //call AppConfig.memberService
+    //call AppConfig.memberRepository
+    //call AppConfig.memberRepository
+    //call AppConfig.orderService
+    //call AppConfig.memberRepository
 
-  @Bean
-  public OrderService orderService() {
-    return new OrderServiceImpl(memberRepository(), discountPolicy());
-  }
+    @Bean
+    public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
+        return new MemberServiceImpl(memberRepository());
+    }
 
-  @Bean
-  public DiscountPolicy discountPolicy() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
+    public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy() {
 //    return new FixDiscountPolicy();
-    return new RateDiscountPolicy();
-  }
+        return new RateDiscountPolicy();
+    }
 }
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,19 +6,24 @@ import hello.core.member.MemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-  private final MemberRepository memberRepository;
-  private final DiscountPolicy discountPolicy;
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
 
-  public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-    this.memberRepository = memberRepository;
-    this.discountPolicy = discountPolicy;
-  }
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
-  @Override
-  public Order createOrder(Long memberId, String itemName, int itemPrice) {
-    Member member = memberRepository.findById(memberId);
-    int discountPrice = discountPolicy.discount(member, itemPrice);
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
 
-    return new Order(memberId, itemName, itemPrice, discountPrice);
-  }
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+
+    // 테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
 }
